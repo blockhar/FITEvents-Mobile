01f81cb50f4a8b8019e3d499786c099b3c7a7884
Bradley Lockhart <blockhar@ualberta.ca>
2017-08-03 18:30:33 -0600

    Add global styles for button, entry, label.

--- Classes/Globals.cs
+++ Classes/Globals.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using RestSharp;
+using Xamarin.Forms;
 
 namespace FITEvents.Classes
 {
@@ -23,4 +24,33 @@ namespace FITEvents.Classes
         static public List<Task> taskList;
     }
 
+    static class FITEventStyles
+    {
+        static public Style buttonStyle = new Style(typeof(Button))
+        {
+            Setters =
+            {
+                new Setter {Property = Button.BackgroundColorProperty, Value = Color.FromRgb(241,184,45) },
+                new Setter {Property = Button.TextColorProperty, Value = Color.White },
+                new Setter {Property = View.MarginProperty, Value = new Thickness(10)}
+            }
+        };
+
+        static public Style entryStyle = new Style(typeof(Entry))
+        {
+            Setters =
+            {
+                new Setter {Property = Entry.TextColorProperty, Value = Color.White},
+                new Setter {Property = Entry.PlaceholderColorProperty, Value = Color.FromRgb(173,173,173)}
+            }
+        };
+
+        static public Style labelStyle = new Style(typeof(Label))
+        {
+            Setters =
+            {
+                new Setter {Property = Label.TextColorProperty, Value = Color.FromRgb(241,184,45)}
+            }
+        };
+    }
 }
